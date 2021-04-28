8403b5b4769d8766c5c800e4cdb29730fcb45377
Karina Finegan <kairosity@users.noreply.github.com>
2021-04-28 13:25:34 +0100

    feat: first product views created with template

--- lionize/urls.py
+++ lionize/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
-    path('hello-webpack/', TemplateView.as_view(template_name='hello_webpack.html'))
+    path('hello-webpack/', TemplateView.as_view(template_name='hello_webpack.html')),
+    path('products/', include('products.urls')),
 ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+
+def all_products(request):
+    '''
+    A view to show all products.
+    '''
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
+
+def content_creation_products(request):
+    '''
+    A view to show all content creation products
+    '''
+
+    return render(request)
