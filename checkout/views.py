7a05b824f026cce1ea205738d7f150b9f9b8e1ec
Karina Finegan <kairosity@users.noreply.github.com>
2021-05-04 19:00:11 +0100

    feat: stripe basic connection functional

--- checkout/views.py
+++ checkout/views.py
@@ -1,20 +1,33 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 
+from django.conf import settings
+
 from .forms import OrderForm
+from bag.contexts import bag_contents
+
+import os
+import stripe
 
 def checkout(request):
+    stripe_public_key = os.getenv('STRIPE_PUBLIC_KEY')
+    stripe_secret_key = os.getenv('STRIPE_SECRET_KEY')
+
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There's nothing in your bag at the moment.")
         return redirect(reverse('products'))
     
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
+    
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51InQT1G54ev3bb3zz0PsP9ohVaKvdXgbgusNmlsEDYHdDZScHQEJ5GzfEltSJ37aotzc45iAENmm4vicxgMOk5CY00DDXyQNXU',
-        'client_secret': 'not a real password',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': stripe_secret_key,
     }
 
     return render(request, template, context)
