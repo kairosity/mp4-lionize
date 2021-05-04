bbef064033cc2ae059cf99082a6f8e9d0e2e45e6
Karina Finegan <kairosity@users.noreply.github.com>
2021-05-04 18:03:27 +0100

    feat: stripe integration started

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51InQT1G54ev3bb3zz0PsP9ohVaKvdXgbgusNmlsEDYHdDZScHQEJ5GzfEltSJ37aotzc45iAENmm4vicxgMOk5CY00DDXyQNXU',
+        'client_secret': 'not a real password',
     }
 
     return render(request, template, context)
--- lionize/settings.py
+++ lionize/settings.py
@@ -166,12 +166,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = 'static'
 STATIC_URL = '/static/'
 MEDIA_ROOT = 'media'
 MEDIA_URL = '/media/'
 
-# STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static') ] # This is the default - not needed.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+    )
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_FINDERS = [ 'django.contrib.staticfiles.finders.FileSystemFinder', #Without this and...
                         'django.contrib.staticfiles.finders.AppDirectoriesFinder', # This, the admin css styles disappear.
