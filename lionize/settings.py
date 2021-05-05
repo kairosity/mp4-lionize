f0535bad9e1476bd968f82fac1a0f17a931a25ab
Karina Finegan <kairosity@users.noreply.github.com>
2021-05-05 13:56:30 +0100

    feat: add class methods to webhook handlers and views

--- lionize/settings.py
+++ lionize/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['7343a1fe6661.ngrok.io']
 
 
 # Application definition
