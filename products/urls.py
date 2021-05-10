dc833904e74025e561efffc8b4d101df7993037a
Karina Finegan <kairosity@users.noreply.github.com>
2021-05-10 17:56:36 +0100

    admin: add product view, url & template

--- products/urls.py
+++ products/urls.py
@@ -3,8 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products, name='products'),
-    path('seo-products', views.seo, name='seo-products'),
-    path('content-creation-products', views.content_creation_products, name='content-creation-products'),
-    path('web-design-products', views.webdesign, name='web-design-products'),
-    path('<int:product_id>', views.product_detail, name='product_detail'),
+    path('seo-products/', views.seo, name='seo-products'),
+    path('content-creation-products/', views.content_creation_products, name='content-creation-products'),
+    path('web-design-products/', views.webdesign, name='web-design-products'),
+    path('<int:product_id>/', views.product_detail, name='product_detail'),
+    path('add/', views.add_product, name='add_product'),
 ]
