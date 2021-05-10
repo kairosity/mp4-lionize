d66e9ec2431e1a774d4e5e5e6add0de169e83d62
Karina Finegan <kairosity@users.noreply.github.com>
2021-05-10 18:04:30 +0100

    feat: add product functionality completed bar toast messages edit

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower 
@@ -83,10 +83,18 @@ def product_detail(request, product_id):
 
 
 def add_product(request):
-    '''
-    Add a product to the store
-    '''
-    form = ProductForm()
+    """ Add a product to the store """
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('add_product'))
+        else:
+            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+    else:
+        form = ProductForm()
+        
     template = 'products/add_product.html'
     context = {
         'form': form,
