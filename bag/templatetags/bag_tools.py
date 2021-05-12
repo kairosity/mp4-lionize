5750b552befb283328a184478c3b70e32509c271
Karina Finegan <kairosity@users.noreply.github.com>
2021-05-12 13:14:59 +0100

    feat: add 2 new filters to shopping bag template: vat & subtotal incl. vat

--- bag/templatetags/bag_tools.py
+++ bag/templatetags/bag_tools.py
@@ -6,3 +6,11 @@ register = template.Library()
 @register.filter(name='calc_subtotal')
 def calc_subtotal(price, quantity):
     return price * quantity
+
+@register.filter(name='vat')
+def vat(price):
+    return float(price) * float(0.23)
+
+@register.filter(name='subtotalinclvat')
+def subtotalinclvat(price, quantity):
+    return  float(price) * float(quantity)  * float(1.23)
